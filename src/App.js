da669ec546e363b5ab6b40bcdcd2785e92b1b02c
EBEREGIT <samsoneberenjoku@gmail.com>
2020-07-06 14:42:33 +0100

    nav animation complete

--- src/App.js
+++ src/App.js
@@ -1,75 +1,97 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 
 function App() {
   return (
     <div className="container text-center">
       {/* <!-- nav --> */}
-    <nav className="navbar navbar-default">
-      <div className="navbar-header">
-        <h1>Navigation</h1>
-      </div>
-    </nav>
-
-    {/* <!-- header --> */}
-    <header>
-      <div className="jumbotron">
-        <h1>Header</h1>
-      </div>
-    </header>
-
-    {/* <!-- main --> */}
-    <main>
-      {/* <!-- services --> */}
-      <section id="services">
-        <h1>Services</h1>
-        <div className="row">
-          <div className="col-xs-12 col-sm-4">
-            <div className="well well-lg"><h1>1</h1></div>
-          </div>
-          <div className="col-xs-12 col-sm-4">
-            <div className="well well-lg"><h1>2</h1></div>
-          </div>
-          <div className="col-xs-12 col-sm-4">
-            <div className="well well-lg"><h1>3</h1></div>
-          </div>
+      <nav
+        className="navbar navbar-default"
+        data-aos="flip-left"
+        data-aos-delay="50"
+        data-aos-duration="2000"
+        data-aos-easing="ease-in-out-cubic"
+      >
+        <div className="navbar-header">
+          <h1>Navigation</h1>
         </div>
-      </section>
+      </nav>
 
-      {/* <!-- about --> */}
-      <section id="about">
-        <h1>About</h1>
-        <div className="row">
-          <div className="col-xs-12 col-sm-6 col-md-8">
-            <div className="well well-lg"><h1>4</h1></div>
-          </div>
-          <div className="col-xs-12 col-sm-6 col-md-4">
-            <div className="well well-lg"><h1>5</h1></div>
-          </div>
+      {/* <!-- header --> */}
+      <header>
+        <div className="jumbotron">
+          <h1>Header</h1>
         </div>
-      </section>
+      </header>
 
-      {/* <!-- contact --> */}
-      <section id="contact">
-        <h1>Contact</h1>
-        <div className="row">
-          <div className="col-xs-12 col-sm-6 col-md-6">
-            <div className="well well-lg"><h1>6</h1></div>
+      {/* <!-- main --> */}
+      <main>
+        {/* <!-- services --> */}
+        <section id="services">
+          <h1>Services</h1>
+          <div className="row">
+            <div className="col-xs-12 col-sm-4">
+              <div className="well well-lg">
+                <h1>1</h1>
+              </div>
+            </div>
+            <div className="col-xs-12 col-sm-4">
+              <div className="well well-lg">
+                <h1>2</h1>
+              </div>
+            </div>
+            <div className="col-xs-12 col-sm-4">
+              <div className="well well-lg">
+                <h1>3</h1>
+              </div>
+            </div>
           </div>
-          <div className="col-xs-12 col-sm-6 col-md-6">
-            <div className="well well-lg"><h1>7</h1></div>
+        </section>
+
+        {/* <!-- about --> */}
+        <section id="about">
+          <h1>About</h1>
+          <div className="row">
+            <div className="col-xs-12 col-sm-6 col-md-8">
+              <div className="well well-lg">
+                <h1>4</h1>
+              </div>
+            </div>
+            <div className="col-xs-12 col-sm-6 col-md-4">
+              <div className="well well-lg">
+                <h1>5</h1>
+              </div>
+            </div>
           </div>
+        </section>
+
+        {/* <!-- contact --> */}
+        <section id="contact">
+          <h1>Contact</h1>
+          <div className="row">
+            <div className="col-xs-12 col-sm-6 col-md-6">
+              <div className="well well-lg">
+                <h1>6</h1>
+              </div>
+            </div>
+            <div className="col-xs-12 col-sm-6 col-md-6">
+              <div className="well well-lg">
+                <h1>7</h1>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+
+      {/* <!-- footer --> */}
+      <footer>
+        <div className="jumbotron">
+          <h1>Footer</h1>
         </div>
-      </section>
-    </main>
-
-    {/* <!-- footer --> */}
-    <footer>
-      <div className="jumbotron">
-        <h1>Footer</h1>
-      </div>
-    </footer>
-
+      </footer>
     </div>
   );
 }
