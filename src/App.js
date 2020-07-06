9c5b53bfc575aa8ca47e8ac41bd1b844b6d21bc9
EBEREGIT <samsoneberenjoku@gmail.com>
2020-07-06 14:52:07 +0100

    Other animations complete

--- src/App.js
+++ src/App.js
@@ -21,7 +21,12 @@ function App() {
       </nav>
 
       {/* <!-- header --> */}
-      <header>
+      <header
+        data-aos="zoom-out-up"
+        data-aos-delay="50"
+        data-aos-duration="2000"
+        data-aos-easing="ease-in-out-cubic"
+      >
         <div className="jumbotron">
           <h1>Header</h1>
         </div>
@@ -33,17 +38,35 @@ function App() {
         <section id="services">
           <h1>Services</h1>
           <div className="row">
-            <div className="col-xs-12 col-sm-4">
+            <div
+              className="col-xs-12 col-sm-4"
+              data-aos="zoom-out-down"
+              data-aos-delay="50"
+              data-aos-duration="2000"
+              data-aos-easing="ease-in-out-cubic"
+            >
               <div className="well well-lg">
                 <h1>1</h1>
               </div>
             </div>
-            <div className="col-xs-12 col-sm-4">
+            <div
+              className="col-xs-12 col-sm-4"
+              data-aos="zoom-out-left"
+              data-aos-delay="50"
+              data-aos-duration="2000"
+              data-aos-easing="ease-in-out-cubic"
+            >
               <div className="well well-lg">
                 <h1>2</h1>
               </div>
             </div>
-            <div className="col-xs-12 col-sm-4">
+            <div
+              className="col-xs-12 col-sm-4"
+              data-aos="zoom-out-right"
+              data-aos-delay="50"
+              data-aos-duration="2000"
+              data-aos-easing="ease-in-out-cubic"
+            >
               <div className="well well-lg">
                 <h1>3</h1>
               </div>
@@ -55,12 +78,24 @@ function App() {
         <section id="about">
           <h1>About</h1>
           <div className="row">
-            <div className="col-xs-12 col-sm-6 col-md-8">
+            <div
+              className="col-xs-12 col-sm-6 col-md-8"
+              data-aos="fade-down"
+              data-aos-easing="linear"
+              data-aos-duration="1500"
+              data-aos-delay="50"
+            >
               <div className="well well-lg">
                 <h1>4</h1>
               </div>
             </div>
-            <div className="col-xs-12 col-sm-6 col-md-4">
+            <div
+              className="col-xs-12 col-sm-6 col-md-4"
+              data-aos="fade-up"
+              data-aos-easing="linear"
+              data-aos-duration="1500"
+              data-aos-delay="50"
+            >
               <div className="well well-lg">
                 <h1>5</h1>
               </div>
@@ -72,12 +107,24 @@ function App() {
         <section id="contact">
           <h1>Contact</h1>
           <div className="row">
-            <div className="col-xs-12 col-sm-6 col-md-6">
+            <div
+              className="col-xs-12 col-sm-6 col-md-6"
+              data-aos="fade-zoom-in"
+              data-aos-easing="ease-in-back"
+              data-aos-delay="300"
+              data-aos-offset="0"
+            >
               <div className="well well-lg">
                 <h1>6</h1>
               </div>
             </div>
-            <div className="col-xs-12 col-sm-6 col-md-6">
+            <div
+              className="col-xs-12 col-sm-6 col-md-6"
+              data-aos="fade-zoom-out"
+              data-aos-easing="ease-in-back"
+              data-aos-delay="1000"
+              data-aos-offset="0"
+            >
               <div className="well well-lg">
                 <h1>7</h1>
               </div>
@@ -88,7 +135,13 @@ function App() {
 
       {/* <!-- footer --> */}
       <footer>
-        <div className="jumbotron">
+        <div
+          className="jumbotron"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+          data-aos-easing="ease-in-back"
+              data-aos-delay="1000"
+        >
           <h1>Footer</h1>
         </div>
       </footer>
